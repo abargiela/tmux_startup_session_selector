351f027a0056c294e2840a99adba8135597fee4a
Alexandre <abargiela@MacBook-Pro.local>
2020-03-30 17:29:00 -0300

    Add script and readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# iterm2_startup_session_selector.sh
-tmux startup selector
+- [Usage:](#usage)
+  - [iterm2:](#iterm2)
+  - [vscode integrated terminal osx](#vscode-integrated-terminal-osx)
+  - [Description](#description)
+  - [Example](#example)
+
+
+# Usage: 
+
+## iterm2:
+
+In `Profiles -> General -> Command ` 
+
+choose: `login shell` and add the path where this script is: `bash /path/to/script/tmux_startup_session_selector.sh`
+
+## vscode integrated terminal osx
+
+Open `setting.json` and add: 
+`"terminal.integrated.shellArgs.osx": ["/path/to/script/tmux_startup_session_selector.sh" ]`
+
+
+## Description
+
+The main idea behind this script was to facilitate when I open a new terminal, so I have an easy way to choose a new or an existent session of tmux without type commands, so it's helpful for me, hope it can help you, suggestions are welcome.
+
+
+## Example
+![Alt Text](./iterm.gif)
---
+++ tmux_startup_session_selector.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Usage: In `Profiles -> General -> Command ` choose: `login shell` and add the path where this script is: `bash ~/tmux_startup_session_selector.sh`
+
+#In vscode: Open setting.json and add: 
+#"terminal.integrated.shellArgs.osx": ["/path/to/script/tmux_startup_session_selector.sh" ]
+
+return_opened_sessions_names(){
+    echo -e "
+Currently opened sessions:
+Type the name that you want to connect and press enter:
+$(tmux list-sessions -F '#S')"
+}
+
+attach_session(){
+    read SESSION_NAME
+    tmux attach -t $SESSION_NAME
+    while [[ $? != 0 ]];do
+        attach_session
+    done
+}
+
+create_new_session(){
+    echo -e "
+Creating a new session, type a name and hit enter."
+    read SESSION_NAME
+    tmux new -s $SESSION_NAME
+}
+
+echo -e "Please choose: 
+c - Create a new session
+a - Attach to an existent one
+e - Do not use tmux
+"
+while :
+do
+  read OPTION
+    case ${OPTION} in
+    	c)
+            clear
+            create_new_session
+            break
+    		;;
+    	a)
+            clear
+            return_opened_sessions_names
+            attach_session
+            break
+    		;;
+        e) 
+            break
+            ;;
+        *) 
+            echo "Type a valid option."
+            ;;
+    esac
+done
